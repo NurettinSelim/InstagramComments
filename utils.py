fa63d05f712a26125d383b6df7c1cab543c753fb
NurettinSelim <30979744+NurettinSelim@users.noreply.github.com>
2020-10-15 12:32:08 +0300

    req updated

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
-import requests
 import json
+
+import requests
+
 
 def get_media_id(url):
     r = requests.get(f'https://api.instagram.com/oembed/?callback=&url={url}')
     json_object = json.loads(r.text)
     return json_object["media_id"]
-
-
